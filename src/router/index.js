d9ef441deb69a9b40fde396b5cfa9f97bd669535
ntduong <ntduong2805@gmail.com>
2023-04-07 13:40:04 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,23 @@ import {
 } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [{
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
-}, ];
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true
+    },
+
+];
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
